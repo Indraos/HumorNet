a496e3b658aeaa8b84f5dc10a78e3dfa0ae57f59
Andreas Haupt <a.haupt@me.com>
2020-12-08 00:23:48 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-mkdir -p /tmp/emnlp2017
-curl https://storage.googleapis.com/deepmind-scratchgan-data/train.json --output /tmp/emnlp2017/train.json
-curl https://storage.googleapis.com/deepmind-scratchgan-data/valid.json --output /tmp/emnlp2017/valid.json
-curl https://storage.googleapis.com/deepmind-scratchgan-data/test.json --output /tmp/emnlp2017/test.json
-curl https://storage.googleapis.com/deepmind-scratchgan-data/glove_emnlp2017.txt --output /tmp/emnlp2017/glove_emnlp2017.txt
+mkdir data
+cd data
+wget -c "https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz"
+cd ..
+
+yum install gcc g++
 
 pip3 install -r requirements.txt
 
