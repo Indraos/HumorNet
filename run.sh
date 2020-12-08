3997bcd970dd76dd4413687debeddee201bdbda8
Andreas Haupt <a.haupt@me.com>
2020-12-08 00:20:47 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,6 @@ curl https://storage.googleapis.com/deepmind-scratchgan-data/valid.json --output
 curl https://storage.googleapis.com/deepmind-scratchgan-data/test.json --output /tmp/emnlp2017/test.json
 curl https://storage.googleapis.com/deepmind-scratchgan-data/glove_emnlp2017.txt --output /tmp/emnlp2017/glove_emnlp2017.txt
 
-yum install python3 
-
 pip3 install -r requirements.txt
 
-python3 -m train.py
+jupyter notebook --no-browser port=8888
