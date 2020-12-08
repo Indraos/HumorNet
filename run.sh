91fec31fe90b7d5c04385ddd874876f08222c8c6
Andreas Haupt <a.haupt@me.com>
2020-12-08 00:29:57 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ cd data
 wget -c "https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz"
 cd ..
 
-yum install gcc
+yum install gcc gcc-c++
 
 pip3 install -r requirements.txt
 
